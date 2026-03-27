77ce1b0423e3d4ea55369fff58d5a4f8ff0bd9c3
conkhikho134 <chibao.02092004@gmail.com>
2026-03-27 10:23:42 +0700

    đã có product nhưng cần thêm components ở dưới

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WebThuMuaPheLieu.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
