6aa7ced3a2e1ea2b8d0fd6ca873cd0c066d52319
ProudNguyen <nguyenhuuhao9@gmail.com>
2026-03-31 15:11:13 +0700

    bin and obj

--- obj/Debug/net10.0/WebThuMuaPheLieu.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebThuMuaPheLieu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebThuMuaPheLieu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060de21510e1d679d993e20ced565a0ebb6eb6ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d9cd699d7f1045359ffaed85b277c30bfa3f2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebThuMuaPheLieu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebThuMuaPheLieu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
