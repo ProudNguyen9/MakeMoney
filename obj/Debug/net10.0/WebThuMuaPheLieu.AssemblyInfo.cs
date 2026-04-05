f166e65cb663b53de358d4986cc58c7c7a07545e
ProudNguyen <nguyenhuuhao9@gmail.com>
2026-04-05 15:23:59 +0700

    h

--- obj/Debug/net10.0/WebThuMuaPheLieu.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebThuMuaPheLieu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebThuMuaPheLieu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0a6ca24d7a4aa390c5622e63036bf64106b4b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+153979d800d27162060f13124786661a6d9463ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebThuMuaPheLieu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebThuMuaPheLieu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
